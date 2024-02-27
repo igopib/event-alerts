166deb865a6411cc21164d05679abd444882f337
Gurpreet <gs109690@gmail.com>
2024-02-26 21:40:43 -0600

    chore: eslint and prettier setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: "es5",
+  jsxSingleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
